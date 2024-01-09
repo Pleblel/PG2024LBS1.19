8f8609aa4847e827ef474cef93c0e28b5111c79f
TedRosling <ted.rosling@elev.ga.lbs.se>
2024-01-09 22:26:55 +0100

    Testet som jag just gjorde och nu är det dags att köra!
    YIPPIE

--- Assets/Scripts/weapon.cs
+++ Assets/Scripts/weapon.cs
@@ -7,7 +7,7 @@ public class weapon : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //hej jag heter ted och jag kan ses i github reposit
     }
 
     // Update is called once per frame
