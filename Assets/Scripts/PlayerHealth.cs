74627130182cfd983a64939323c2a7686ba568a3
luxec0re <lucas.palmhager@elev.ga.lbs.se>
2024-01-10 16:30:58 +0100

    skoldag 2

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+
+    public int MaxHealth = 100;
+    public int currenthealth;
+
+
+    public HealthBar healthbar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currenthealth = MaxHealth;
+        healthbar.SetMaxHealth(MaxHealth);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TakeDamage(20);
+        }
+
+
+
+    }
+    void TakeDamage(int damage)
+    {
+        currenthealth -= damage;
+    }
+}
