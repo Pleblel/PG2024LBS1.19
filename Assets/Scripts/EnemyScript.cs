cb3f19519e965022d98872c10e4704e7d8967bf9
mystiified <152400721+mystiified@users.noreply.github.com>
2024-01-10 16:29:29 +0100

    Added basic enemy movement functionality

---
+++ Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+    [SerializeField]
+    GameObject Player;
+    GameObject gameObject;
+
+    private Vector2 vMove;
+    public float mvmSpeedEnemy;
+
+    Rigidbody2D EnemyRigidBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        EnemyRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 dir = Player.transform.position - transform.position;
+        Vector2 dirForce = dir.normalized * mvmSpeedEnemy;
+        EnemyRigidBody.velocity = dirForce;
+
+    }
+}
