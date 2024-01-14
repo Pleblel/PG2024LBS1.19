6d7efe017a6cccec626369d35181cb92ef25af4d
Pleblel <pelleaugust07@outlook.com>
2024-01-14 17:28:38 +0100

    Revert "Revert "Merge pull request #17 from Pleblel/F-Music-(Cooking)""
    This reverts commit fd9af533e0013c0098296bf3624e3eff2f6c468c.

--- Assets/Scripts/playerMove.cs
+++ Assets/Scripts/playerMove.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
+using System.Threading;
 using UnityEngine;
 
 public class playerMove : MonoBehaviour
@@ -14,14 +15,24 @@ public class playerMove : MonoBehaviour
     public float rollingSpeed;
     public float rollingTime;
     public float rollingCooldown;
+    public float RollingVolume;
+    public float WalkingVolume;
+    int counting = 0;
+    [SerializeField]
+    private AudioClip[] SFXWalks; 
+    [SerializeField]
+    private AudioClip SFXDash;
     void Update()
     {
+        //Check roll
         if (isRolling)
         {
             return;
         }
+        //looks for input
         float moveX = Input.GetAxisRaw("Horizontal");
         float moveY = Input.GetAxisRaw("Vertical");
+        //sets direction for movement
         moveDir = new Vector2(moveX, moveY).normalized;
         if (Input.GetButtonDown("Jump") && canRoll)
         {
@@ -31,15 +42,33 @@ public class playerMove : MonoBehaviour
 
     void FixedUpdate()
     {
+        //checks for rolling, if yes. return.
         if (isRolling)
         {
             return;
         }
+        //Move player
         rb.velocity = new Vector2(moveDir.x * moveSpeed, moveDir.y * moveSpeed);
+        
+        //Walk run audio HERE!
+        if (Mathf.Abs(rb.velocity.magnitude) > WalkingVolume)
+        {
+            counting++;
+            if (counting == 10)
+            { 
+                //SoundFXManager.instance.PlayLoopingSFX(SFXWalk);
+                Debug.Log(SoundFXManager.instance);
+                SoundFXManager.instance.PlayRandomSoundFXclip(SFXWalks, transform, 1f);
+                counting = 0;
+            }
+        }
+        
+        
     }
 
     IEnumerator Roll()
     {
+        //code for dash
         canRoll = false;
         isRolling = true;
         rb.velocity += new Vector2(moveDir.x * rollingSpeed, moveDir.y * rollingSpeed );
@@ -47,5 +76,8 @@ public class playerMove : MonoBehaviour
         isRolling = false;
         yield return new WaitForSeconds(rollingCooldown);
         canRoll = true;
+
+        //DASH AUDIO HERE!
+        SoundFXManager.instance.PlaySoundFXclip(SFXDash, transform, RollingVolume);
     }
 }
