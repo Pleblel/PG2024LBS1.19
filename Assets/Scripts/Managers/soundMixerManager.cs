3b17f03e20cb84243d5fd6894810b8a21d3ddcb0
Pleblel <pelleaugust07@outlook.com>
2024-01-16 13:33:11 +0100

    sound mix maneger coding

--- Assets/Scripts/Managers/soundMixerManager.cs
+++ Assets/Scripts/Managers/soundMixerManager.cs
@@ -9,16 +9,19 @@ public class NewBehaviourScript : MonoBehaviour
 
     public void SetMasterVolume(float level)
     {
-        audioMixer.SetFloat("MasterVolume", level);  
+        //audioMixer.SetFloat("MasterVolume", level);
+        audioMixer.SetFloat("MasterVolume", Mathf.Log10(level) * 20);
     }
 
     public void SetSoundFXVolume(float level)
     {
-        audioMixer.SetFloat("MusicVolume", level);
+       // audioMixer.SetFloat("SFXVolume", level);
+        audioMixer.SetFloat("SFXVolume", Mathf.Log10(level) * 20);
     }
 
     public void SetMusicVolume(float level)
     {
-        audioMixer.SetFloat("SFXVolume", level);
+        //audioMixer.SetFloat("MusicVolume", level);
+        audioMixer.SetFloat("MusicVolume", Mathf.Log10(level) * 20);
     }
 }
