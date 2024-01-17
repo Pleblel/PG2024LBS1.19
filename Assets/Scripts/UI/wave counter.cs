3df874d27ec523d2b498e3fa5117610085478762
TedRosling <ted.rosling@elev.ga.lbs.se>
2024-01-17 23:13:23 +0100

    Create wave counter.cs

---
+++ Assets/Scripts/UI/wave counter.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class wavecounter : MonoBehaviour
+{
+    //makes the code be able to work with the text in unity
+    [SerializeField]
+    GameObject WaveCounter;
+    WaveControl waveControl;
+    TextMeshProUGUI textComponent;
+    
+    void Start()
+    {
+        textComponent = WaveCounter.GetComponent<TextMeshProUGUI>();
+        waveControl = FindObjectOfType<WaveControl>();
+    }
+
+    // Updates the counter of waves
+    void Update()
+    {
+        string waves = "Wave: " + (waveControl.currentWaveIndex + 1);
+        textComponent.text = waves;
+    }
+}
+//Made by Lucas och Elm
