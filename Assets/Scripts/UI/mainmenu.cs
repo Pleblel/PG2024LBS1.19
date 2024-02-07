ca22e85a948d7b6051f18dce36db2f24432c7d60
luxec0re <lucas.palmhager@elev.ga.lbs.se>
2024-02-07 13:42:05 +0100

    fully working endless button

--- Assets/Scripts/UI/mainmenu.cs
+++ Assets/Scripts/UI/mainmenu.cs
@@ -4,16 +4,22 @@ using UnityEngine.SceneManagement;
 public class mainmenu : MonoBehaviour
 {
     // Starts the game if u press "play"
-    public void playGame()
+    public void PlayGame()
     {
         SceneManager.LoadScene(2);
 
         WaveControl.start = true;
     }
     // Makes the game close if u press "quit"
-    public void quitgame()
+    public void QuitGame()
     {
         Debug.Log("Quit!");
         Application.Quit();
     }
+    //starts endless mode
+    public void EndlessMode()
+    {
+        SceneManager.LoadScene(3);
+    }
 }
+//gjord av Lucas
