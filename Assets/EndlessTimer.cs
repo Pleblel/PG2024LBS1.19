a6534358ba53c077fa61cff36a5726ad277aadb9
mystiified <152400721+mystiified@users.noreply.github.com>
2024-02-09 22:08:42 +0100

    Added a timer for the Endless mode.

---
+++ Assets/EndlessTimer.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class EndlessTimer : MonoBehaviour
+{
+    float timer = -0.49f;
+    public TextMeshProUGUI textcomponent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textcomponent = GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        textcomponent.text = "You have survived for " + Mathf.Round(timer) + " seconds!";
+    }
+}
